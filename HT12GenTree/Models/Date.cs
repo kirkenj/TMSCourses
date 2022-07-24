dfb77238f16fa4159e3606569c12f0d4ec6a4b5f
kirkenj <kirkenj@bk.ru>
2022-07-24 23:50:26 +0300

    won the fight with date

--- HT12GenTree/Models/Date.cs
+++ HT12GenTree/Models/Date.cs
@@ -3,29 +3,180 @@
     public class Date
     {
         private int _fullDaysAmm = 0;
+        private int _yearNumber;
+        private int _monthNumber;
+        private int _dayNumber;
+
         private const int DAYS_AMM_NOT_LEAP_YEAR = 365;
         private const int DAYS_AMM_LEAP_YEAR = DAYS_AMM_NOT_LEAP_YEAR + 1;
+        private const int DAYS_AMM_4_YEARS_SINCE_DATE_START = DAYS_AMM_NOT_LEAP_YEAR * 4 + 1;
+        private const int DAYS_AMM_100_YEARS_SINCE_DATE_START = 36524;
         private const int DAYS_AMM_400_YEARS_SINCE_DATE_START = 146097;
 
-
-        public static int GetDaysAmmInMonthByNumber(int monthNumber, bool isLeapYear)
+        public Date(int dayNumber, int monthNumber, int yearNumber)
         {
-            return monthNumber switch
+            if (yearNumber <= 0)
             {
-                1 => 31,
-                2 => isLeapYear ? 29 : 28,
-                3 => 31,
-                4 => 30,
-                5 => 31,
-                6 => 30,
-                7 => 31,
-                8 => 31,
-                9 => 30,
-                10 => 31,
-                11 => 30,
-                12 => 31,
-                _ => throw new ArgumentOutOfRangeException(nameof(monthNumber)),
-            };
+                throw new ArgumentOutOfRangeException(nameof(yearNumber));
+            }
+
+            _fullDaysAmm = GetDaysAmmInPrevYears(yearNumber);
+            _yearNumber = yearNumber;
+            bool isLeapYear = IsLeapYear(yearNumber);
+
+            if (monthNumber < 1 || monthNumber > 12)
+            {
+                throw new ArgumentOutOfRangeException(nameof(monthNumber));
+            }
+
+            _fullDaysAmm += GetDaysAmmInPrevMonth(monthNumber, isLeapYear);
+            _monthNumber = monthNumber;
+        
+            if (dayNumber < 1 || dayNumber > GetDaysAmmInMonthByNumber(monthNumber, isLeapYear)) 
+            {
+                throw new ArgumentOutOfRangeException(nameof(dayNumber));
+            }
+
+            _fullDaysAmm += dayNumber - 1;
+            _dayNumber = dayNumber;
+        }
+
+        private void UpdateAll()
+        {
+            var days = _fullDaysAmm;
+            _yearNumber = GetCurrentYearNumberByPassedDaysAmm(days);
+            var viaYears = GetDaysAmmInPrevYears(_yearNumber);
+            days -= viaYears;
+            _monthNumber = GetMonthNumberByPassedDays(days, IsLeapYear(_yearNumber));
+            days -= GetDaysAmmInPrevMonth(_monthNumber, IsLeapYear(_yearNumber));
+            _dayNumber = days + 1;
+        }
+
+        public int Year 
+        { 
+            get => _yearNumber;
+        }
+
+        public int Month
+        {
+            get => _monthNumber;
+        }
+
+        public int Day
+        {
+            get => _dayNumber;
+        }
+
+        public bool IsLeap => IsLeapYear(Year);
+
+        public void AddDays(int days)
+        {
+            if (_fullDaysAmm + days < 0)
+            {
+                throw new ArgumentException("That addition will make the object invalid", nameof(days));
+            }
+
+            _fullDaysAmm += days;
+            UpdateAll();
+        }
+
+        public static int GetDaysAmmInMonthByNumber(int monthNumber, bool isLeapYear) => monthNumber switch
+        {
+            1 => 31,
+            2 => isLeapYear ? 29 : 28,
+            3 => 31,
+            4 => 30,
+            5 => 31,
+            6 => 30,
+            7 => 31,
+            8 => 31,
+            9 => 30,
+            10 => 31,
+            11 => 30,
+            12 => 31,
+            _ => throw new ArgumentOutOfRangeException(nameof(monthNumber)),
+        };
+
+        public static int GetDaysAmmInPrevMonth(int monthNumber, bool isLeapYear) => monthNumber switch
+        {
+            1 => 0,
+            2 => 31,
+            3 => 59,
+            4 => 90,
+            5 => 120,
+            6 => 151,
+            7 => 181,
+            8 => 212,
+            9 => 243,
+            10 => 273,
+            11 => 304,
+            12 => 334,
+            _ => throw new ArgumentOutOfRangeException(nameof(monthNumber))
+        } + (monthNumber > 2 && isLeapYear ? 1 : 0);
+
+        public static int GetMonthNumberByPassedDays(int daysAmm, bool isLeapYear)
+        {
+            if (daysAmm < 0 || (isLeapYear && daysAmm >= 366) || (!isLeapYear && daysAmm >= 365))
+            {
+                throw new ArgumentOutOfRangeException(nameof(daysAmm));
+            }
+
+            if (daysAmm <= 30)
+            {
+                return 1;
+            }
+
+            if (daysAmm <= 58 + (isLeapYear ? 1 : 0))
+            {
+                return 2;
+            }
+
+            if (daysAmm <= 89 + (isLeapYear ? 1 : 0))
+            {
+                return 3;
+            }
+
+            if (daysAmm <= 119 + (isLeapYear ? 1 : 0))
+            {
+                return 4;
+            }
+
+            if (daysAmm <= 150 + (isLeapYear ? 1 : 0))
+            {
+                return 5;
+            }
+            
+            if (daysAmm <= 180 + (isLeapYear ? 1 : 0))
+            {
+                return 6;
+            }
+            
+            if (daysAmm <= 211 + (isLeapYear ? 1 : 0))
+            {
+                return 7;
+            }
+            
+            if (daysAmm <= 242 + (isLeapYear ? 1 : 0))
+            {
+                return 8;
+            }
+            
+            if (daysAmm <= 272 + (isLeapYear ? 1 : 0))
+            {
+                return 9;
+            }
+            
+            if (daysAmm <= 303 + (isLeapYear ? 1 : 0))
+            {
+                return 10;
+            }
+            
+            if (daysAmm <= 333 + (isLeapYear ? 1 : 0))
+            {
+                return 11;
+            }
+
+            return 12;
         }
 
         public static int GetDaysAmmInPrevYears(int yearNumber)
@@ -37,13 +188,13 @@
 
             var yearsAmm = yearNumber - 1;
 
-            int ret = yearsAmm / DAYS_AMM_400_YEARS_SINCE_DATE_START;
-            yearsAmm %= DAYS_AMM_400_YEARS_SINCE_DATE_START;
-            for (int i = 1; i <= yearsAmm; i++)
-            {
-                ret += IsLeapYear(i) ? DAYS_AMM_LEAP_YEAR : DAYS_AMM_NOT_LEAP_YEAR;
-            }
-
+            int ret = (yearsAmm / 400) * DAYS_AMM_400_YEARS_SINCE_DATE_START;
+            yearsAmm %= 400;
+            ret += (yearsAmm / 100) * DAYS_AMM_100_YEARS_SINCE_DATE_START;
+            yearsAmm %= 100;
+            ret += (yearsAmm / 4) * DAYS_AMM_4_YEARS_SINCE_DATE_START;
+            yearsAmm %= 4;
+            ret += yearsAmm * DAYS_AMM_NOT_LEAP_YEAR;
             return ret;
         }
 
@@ -54,16 +205,28 @@
                 throw new ArgumentOutOfRangeException(nameof(year));
             }
 
-            return (year % 100 == 0 && year % 400 == 0) || (year % 100 != 0 && year % 4 == 0);
+            return (year % 400 == 0) || (year % 100 != 0 && year % 4 == 0);
         }
-        public Date(int days, int month, int years)
+
+        public static int GetCurrentYearNumberByPassedDaysAmm(int dayAmm)
         {
-            if (years <= 0)
+            if (dayAmm < 0)
             {
-                throw new ArgumentOutOfRangeException(nameof(years));
+                throw new ArgumentOutOfRangeException(nameof(dayAmm));
             }
 
-
+            int ret = 1;
+            ret += (dayAmm / DAYS_AMM_400_YEARS_SINCE_DATE_START) * 400;
+            dayAmm %= DAYS_AMM_400_YEARS_SINCE_DATE_START;
+            var buffDev = dayAmm / DAYS_AMM_100_YEARS_SINCE_DATE_START;
+            buffDev = buffDev == 4 ? 3 : buffDev;
+            ret += (buffDev) * 100;
+            dayAmm -= DAYS_AMM_100_YEARS_SINCE_DATE_START * buffDev;
+            ret += (dayAmm / DAYS_AMM_4_YEARS_SINCE_DATE_START) * 4;
+            dayAmm %= DAYS_AMM_4_YEARS_SINCE_DATE_START;
+            buffDev = dayAmm / DAYS_AMM_NOT_LEAP_YEAR;
+            ret += buffDev == 4 ? 3 : buffDev;
+            return ret;
         }
     }
 }
--- TestProjectForHT12/DateTests.cs
+++ TestProjectForHT12/DateTests.cs
@@ -52,9 +52,99 @@ namespace TestProjectForHT12
         [TestCase(3, ExpectedResult = 730)]
         [TestCase(4, ExpectedResult = 1095)]
         [TestCase(5, ExpectedResult = 1461)]
+        [TestCase(31, ExpectedResult = 10957)]
         [TestCase(101, ExpectedResult = 36524)]
+        [TestCase(131, ExpectedResult = 47481)]
         [TestCase(401, ExpectedResult = 146097)]
         [TestCase(1201, ExpectedResult = 438291)]
+        [TestCase(1231, ExpectedResult = 449248)]
+        [TestCase(1331, ExpectedResult = 485772)]
         public int DateGetDaysAmmInPrevYears_Returnsvalue(int yearNumber) => Date.GetDaysAmmInPrevYears(yearNumber);
+
+        [TestCase(1, false, ExpectedResult = 0)]
+        [TestCase(2, false, ExpectedResult = 31)]
+        [TestCase(3, false, ExpectedResult = 59)]
+        [TestCase(4, false, ExpectedResult = 90)]
+        [TestCase(5, false, ExpectedResult = 120)]
+        [TestCase(6, false, ExpectedResult = 151)]
+        [TestCase(7, false, ExpectedResult = 181)]
+        [TestCase(8, false, ExpectedResult = 212)]
+        [TestCase(9, false, ExpectedResult = 243)]
+        [TestCase(10, false, ExpectedResult = 273)]
+        [TestCase(11, false, ExpectedResult = 304)]
+        [TestCase(12, false, ExpectedResult = 334)]
+        [TestCase(3, true, ExpectedResult = 60)]
+        [TestCase(4, true, ExpectedResult = 91)]
+        [TestCase(5, true, ExpectedResult = 121)]
+        [TestCase(6, true, ExpectedResult = 152)]
+        [TestCase(7, true, ExpectedResult = 182)]
+        [TestCase(8, true, ExpectedResult = 213)]
+        [TestCase(9, true, ExpectedResult = 244)]
+        [TestCase(10, true, ExpectedResult = 274)]
+        [TestCase(11, true, ExpectedResult = 305)]
+        [TestCase(12, true, ExpectedResult = 335)]        
+        public int DateGetDaysAmmInPrevMonth_Returnsvalue(int monthNumber, bool isLeap) => Date.GetDaysAmmInPrevMonth(monthNumber, isLeap);
+ 
+        [TestCase(1, false)]
+        [TestCase(2, false)]
+        [TestCase(3, false)]
+        [TestCase(4, false)]
+        [TestCase(5, false)]
+        [TestCase(6, false)]
+        [TestCase(7, false)]
+        [TestCase(8, false)]
+        [TestCase(9, false)]
+        [TestCase(10, false)]
+        [TestCase(11, false)]
+        [TestCase(12, false)]
+        [TestCase(3, true)]
+        [TestCase(4, true)]
+        [TestCase(5, true)]
+        [TestCase(6, true)]
+        [TestCase(7, true)]
+        [TestCase(8, true)]
+        [TestCase(9, true)]
+        [TestCase(10, true)]
+        [TestCase(11, true)]
+        [TestCase(12, true)]        
+        public void DateGetMonthAmm_TestVia_DateGetDaysAmmInPrevMonth(int monthNumber, bool isLeap)
+        {
+            var days = Date.GetDaysAmmInPrevMonth(monthNumber, isLeap) + 15;
+            Assert.AreEqual(monthNumber, Date.GetMonthNumberByPassedDays(days, isLeap));
+        }
+
+
+        [TestCase(1, ExpectedResult = 1)]
+        [TestCase(365, ExpectedResult = 2)]
+        [TestCase(730, ExpectedResult = 3)]
+        [TestCase(1095, ExpectedResult = 4)]
+        [TestCase(1461, ExpectedResult = 5)]
+        [TestCase(10957, ExpectedResult = 31)]
+        [TestCase(36524, ExpectedResult = 101)]
+        [TestCase(47481, ExpectedResult = 131)]
+        [TestCase(146097, ExpectedResult = 401)]
+        [TestCase(146096, ExpectedResult = 400)]
+        [TestCase(438291, ExpectedResult = 1201)]
+        [TestCase(449248, ExpectedResult = 1231)]
+        [TestCase(485772, ExpectedResult = 1331)]
+        [TestCase(1460, ExpectedResult = 4)]
+        public int DateGetCurrentYearByPassedDaysAmm_Returnsvalue(int dayAmm) => Date.GetCurrentYearNumberByPassedDaysAmm(dayAmm);
+
+        [TestCase(ExpectedResult = true)]
+        public bool MegaTest()
+        {
+            DateTime date = new (1,1,1);
+            Date dateUser = new (1,1,1);
+            for (int i = 0; i < (date - DateTime.MaxValue).Days; i++)
+            {
+                if (!(date.Year == dateUser.Year && date.Month == dateUser.Month && date.Day == dateUser.Day))
+                {
+                    return false;
+                }
+                date = date.AddDays(1);
+                dateUser.AddDays(1);
+            }
+            return true;
+        }
     }
 }
