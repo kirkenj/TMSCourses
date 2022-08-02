77cd2e2fd4cea9f96b7d1ecbe8ae640f7e1286f5
kirkenj <kirkenj@bk.ru>
2022-08-02 09:49:08 +0300

    eow

---
+++ HT13/Domain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HT13
+{
+    public static class Domain
+    {
+        public static string Name { get; set; }
+
+        public static virtual override string ToString() => $"Domain - {Name}";
+
+        public Domain(string name)
+        {
+            Name = name;
+        }
+    }
+}
