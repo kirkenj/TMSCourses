c2ca13ffdcb1008c430f345f76c67cd9c74b9a7a
kirkenj <kirkenj@bk.ru>
2022-08-03 15:54:17 +0300

    testing 'new'

---
+++ HT14/Program.cs
@@ -0,0 +1,44 @@
+﻿var cat = new Cat("Meow");
+Animal kitten = new Kitten("eeeee");
+cat.Speak();
+kitten.Speak();
+
+
+public class Animal
+{
+    string Sound { get; set; }
+
+    public Animal(string sound)
+    {
+        Sound = sound;
+    }
+
+    public virtual void Speak()
+    {
+        Console.WriteLine(Sound);
+    }
+}
+
+
+public class Cat : Animal
+{
+    public Cat(string sound) : base(sound)
+    {
+
+    }
+
+    new public void Speak()
+    {
+        Console.WriteLine("MEOW");
+    }
+}
+
+public class Kitten : Cat
+{
+    public Kitten(string sound) : base(sound)
+    {
+
+    }
+}
+
+
