6f456d1a57b6e0945fb2c80b8be881839703a1fe
kirkenj <kirkenj@bk.ru>
2022-10-07 00:21:48 +0300

    Switched to the source Db

--- WEB_EF/Models/DBContexts/AutoparkContext.cs
+++ WEB_EF/Models/DBContexts/AutoparkContext.cs
@@ -28,7 +28,7 @@ namespace WEB_EF.Models.DBContexts
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-9VGHPR7\\SQLEXPRESS;Database=AutoparkCF;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=DESKTOP-9VGHPR7\\SQLEXPRESS;Database=Autopark;Trusted_Connection=True;");
             }
         }
 
