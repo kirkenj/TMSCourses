eaa4af1e0c9fc096dfcf7c066cf779376906a6be
kirkenj <kirkenj@bk.ru>
2022-11-11 00:21:22 +0300

    commit just to be

---
+++ WebAPITestProject/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Xunit;
+using WebApi.Models.Services;
+
+namespace WebAPITestProject
+{
+    public class UnitTest1
+    {
+        
+
+        //JournalCRUDLService sut = new JournalCRUDLService();
+
+        [Fact]
+        public void Test1()
+        {
+            //Assert.
+        }
+    }
+}
