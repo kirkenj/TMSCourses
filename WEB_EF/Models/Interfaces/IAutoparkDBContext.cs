a7935211108bf15224b5ef53d28e326737188926
kirkenj <kirkenj@bk.ru>
2022-11-11 00:29:15 +0300

    Fixed WEB_EF project

--- WEB_EF/Models/Interfaces/IAutoparkDBContext.cs
+++ WEB_EF/Models/Interfaces/IAutoparkDBContext.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using WebApiDatabase.Entities;
+
+namespace WebApi.Models.Interfaces
+{
+    public interface IAutoparkDBContext
+    {
+        DbSet<Car> Cars { get; }
+        DbSet<CarType> CarTypes { get; }
+        DbSet<Client> Clients { get; }
+        DbSet<Journal> Journals { get; }
+        DbSet<ParkingPlace> ParkingPlaces { get; }
+
+        public int SaveChanges();
+        EntityEntry<TEntity> Entry<TEntity>(TEntity obj) where TEntity : class;
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
+
